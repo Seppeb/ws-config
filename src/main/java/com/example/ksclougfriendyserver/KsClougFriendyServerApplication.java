fb5a7bede9036d7fd7d0ff443cd6f1265b8cf7bb
Seppe Berghmans <seppeberghmans@gmail.com>
2019-03-25 16:48:12 +0100

    sdf

---
+++ src/main/java/com/example/ksclougfriendyserver/KsClougFriendyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ksclougfriendyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsClougFriendyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsClougFriendyServerApplication.class, args);
+	}
+
+}
